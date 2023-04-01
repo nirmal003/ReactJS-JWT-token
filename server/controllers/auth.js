dca7fdf97d56ae19025f8f249e21eb55833229fc
Nirmal <nirmalraj1025@gmail.com>
2023-04-01 17:50:14 +0530

    token auth

---
+++ server/controllers/auth.js
@@ -0,0 +1,63 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import users from "../models/users.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    const existinguser = await users.findOne({ email });
+
+    if (existinguser) {
+      return res.status(404).json({ message: "User already Exist." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 4);
+
+    const newUser = await users.create({
+      name: fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    console.log(newUser);
+
+    const token = jwt.sign(
+      { email: newUser.email, id: newUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result: newUser, token });
+  } catch (error) {
+    res.status(500).json("Something went wrong...");
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existinguser = await users.findOne({ email });
+
+    if (!existinguser) {
+      return res.status(404).json({ message: "User doesn't Exist." });
+    }
+
+    const isPasswordCrt = await bcrypt.compare(password, existinguser.password);
+
+    if (!isPasswordCrt) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { email: existinguser.email, id: existinguser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result: existinguser, token });
+  } catch (error) {
+    res.status(500).json("Something went wrong...");
+  }
+};
